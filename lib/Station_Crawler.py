a6cd2da9b756667ab43352ae4d4bf7dc61033e99
titangene <titangene.tw@gmail.com>
2018-10-02 14:25:06 +0800

    refactor: station crawler: json to dataframe

--- lib/Station_Crawler.py
+++ lib/Station_Crawler.py
@@ -15,9 +15,9 @@ class Station_Crawler:
 		js_str = self.parse_js(js_str)
 
 		json_data = self.parse_json(js_str)
-		climate_station_dict = self.data_to_dict(json_data)
-		self.climate_station_df = self.data_to_dataFrame(climate_station_dict)
-		self.save_data_to_csv()
+		station_df = self.json_to_dataFrame(json_data)
+
+		self.save_df_to_csv(station_df)
 
 	def parse_js(self, js_str):
 		# 去除多餘的換行和 tab
@@ -34,28 +34,19 @@ class Station_Crawler:
 		json_data = json.loads(json_str)
 		return json_data
 
-	def data_to_dict(self, data):
-		climate_station_dict = {'station_id': [], 'station_name': [], 'location': []}
+	# 資料來源 e.g. {"466880":["板橋", "BANQIAO", "新北市", "1"], "467780": ["七股", "QIGU", "臺南市", "2"]}
+	# 資料內的最後一個參數 "2" 代表此觀測站已撤銷 (cancellation_station)，因此不紀錄此觀測站
+	# output columns: station_id, station_name, location
+	def json_to_dataFrame(self, json_data):
+		df_columns = ['station_name', 'station_name_eng', 'location', 'cancellation_station']
+		station_df = pd.DataFrame.from_dict(json_data, orient='index', columns=df_columns)\
+								 .reset_index()\
+								 .rename(columns={'index': 'station_id'})\
+								 .query('cancellation_station == "1"')\
+								 .drop(['station_name_eng', 'cancellation_station'], axis=1)
+		return station_df
 
-		for key, value in data.items():
-			cancellation_station = value[3]
-			# e.g. '467780': ['七股', 'QIGU', '臺南市', '2']
-			# 資料內的最後一個參數 "2" 代表此觀測站已撤銷，因此不紀錄此觀測站
-			if (cancellation_station == '2'):
-				continue
-			station_id = key
-			station_name = value[0]
-			location = value[2]
-			climate_station_dict['station_id'].append(station_id)
-			climate_station_dict['station_name'].append(station_name)
-			climate_station_dict['location'].append(location)
-		return climate_station_dict
-
-	def data_to_dataFrame(self, climate_station_dict):
-		climate_station_df = pd.DataFrame(climate_station_dict)
-		return climate_station_df
-
-	def save_data_to_csv(self):
+	def save_df_to_csv(self, station_df):
 		file_path = 'data/climate_station.csv'
-		self.climate_station_df.to_csv(file_path, encoding='utf-8', index=False)
+		station_df.to_csv(file_path, encoding='utf-8', index=False)
 		print('save: {}'.format(file_path))
