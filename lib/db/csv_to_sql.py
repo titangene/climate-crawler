555138bb2f4e73a31a6c0c5abd596fb208ca6eeb
titangene <titangene.tw@gmail.com>
2018-10-16 08:52:27 +0800

    feat: DataFrame.to_sql() can set primary key and not null

--- lib/db/csv_to_sql.py
+++ lib/db/csv_to_sql.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine, DateTime, Date, VARCHAR
+from sqlalchemy import create_engine, Table, Column, DateTime, CHAR, NCHAR
+from sqlalchemy.schema import MetaData
+from pandas.io.sql import SQLDatabase, SQLTable
 import pandas as pd
 import numpy as np
 
@@ -42,15 +44,46 @@ class csv_to_mssql:
 		dataSet = self.load_csv(csv_name)
 		self.to_sql(dataSet, table_name, if_exists)
 
-	def to_sql(self, dataSet, table_name, if_exists, dtype=None):
+	def to_sql(self, dataSet, table_name, if_exists, dtype=None, keys=None, sql_table=None):
 		result = "This Dataset had been storaged in DB"
 		try:
-			dataSet.to_sql(table_name, self.engine, if_exists=if_exists, index=False, dtype=dtype)
+			if keys is not None and sql_table is not None:
+				dataSet.to_sql(table_name, self.engine, if_exists=if_exists, index=False, dtype=dtype)
+			if keys is None and sql_table is None:
+				self.to_sql_set_primary_key_and_not_null(dataSet, table_name, self.engine,
+						if_exists=if_exists, index=False, keys=keys, sql_table=sql_table)
 		except Exception as e:
 			result = e
 		else:
 			result = '{} ({}): OKAY'.format(table_name, if_exists)
 		print(result)
+
+	# DataFrame.to_sql() 自訂版
+	# 可自訂 primary key 和 not null
+	def to_sql_set_primary_key_and_not_null(self, frame, name, con, keys, sql_table,
+			schema=None, if_exists='fail', index=True, index_label=None, chunksize=None, dtype=None):
+		# ref: https://github.com/pandas-dev/pandas/blob/master/pandas/io/sql.py#L437
+		if if_exists not in ('fail', 'replace', 'append'):
+			raise ValueError("'{0}' is not valid for if_exists".format(if_exists))
+
+		# ref: https://github.com/pandas-dev/pandas/blob/master/pandas/io/sql.py#L508
+		pandas_sql = SQLDatabase(con, schema=schema)
+
+		if isinstance(frame, pd.Series):
+			frame = frame.to_frame()
+		elif not isinstance(frame, pd.DataFrame):
+			raise NotImplementedError("'frame' argument should be either a Series or a DataFrame")
+
+		if dtype is not None:
+			from sqlalchemy.types import to_instance, TypeEngine
+			for col, my_type in dtype.items():
+				if not isinstance(to_instance(my_type), TypeEngine):
+					raise ValueError('The type of {} is not a SQLAlchemy type '.format(col))
+
+		table = SQLTable(name, pandas_sql, frame=frame, index=index, if_exists=if_exists, index_label=index_label, schema=schema, keys=keys, dtype=dtype)
+		table.table = sql_table
+		table.create()
+		table.insert(chunksize)
 
 	# 處理 日 和 小時 氣候資料
 	def deal_with_daily_and_hourly_data(self):
