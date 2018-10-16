e169c12347084a7eda1ee290eb079827d81fa6b3
titangene <titangene.tw@gmail.com>
2018-10-16 15:50:23 +0800

    refactor: climate station and init set station name encodeURI

--- lib/Climate_Station.py
+++ lib/Climate_Station.py
@@ -5,8 +5,12 @@ from urllib.parse import quote
 class Climate_Station:
 	def __init__(self):
 		self.base_url = 'https://e-service.cwb.gov.tw/HistoryDataQuery'
+		self.hourly_url = 'DayDataController.do?command=viewMain'
+		self.daily_url = 'MonthDataController.do?command=viewMain'
 		self.dataFrame = self.read_station_csv_to_df()
 		self.all_station_id = self.get_all_station_id()
+
+		self.dataFrame['stname'] = self.dataFrame['station_name'].apply(lambda name: self.set_stname(name))
 
 	# 讀取觀測站 csv
 	def read_station_csv_to_df(self):
@@ -21,10 +25,10 @@ class Climate_Station:
 
 	def encodeURI(self, uri):
 		# 包括 '/', '(', ')' 不會做 encode 處理
-		return quote(uri, safe='/\(\)')
+		return quote(uri, safe=r'/\(\)')
 
 	# 將觀測站名稱做兩次 encode 處理
-	def get_stname(self, station_name):
+	def set_stname(self, station_name):
 		return self.encodeURI(self.encodeURI(station_name))
 
 	# 用 觀測站 id 找到對應的 觀測站名稱
@@ -32,25 +36,6 @@ class Climate_Station:
 		expression = 'station_id == "{}"'.format(station_id)
 		station_name = self.dataFrame.query(expression)['station_name'][0]
 		return station_name
-
-	# 用 觀測站名稱 找到對應的 觀測站 id
-	def get_station_id(self, station_name):
-		expression = 'station_name == "{}"'.format(station_name)
-		station_id = self.dataFrame.query(expression).index[0]
-		return station_id
-
-	# 取得 觀測站 id、名稱、encode
-	def get_station(self, station_id=None, station_name=None):
-		# 若輸入 觀測站名稱，但未輸入 觀測站 id，會用 觀測站名稱 找 觀測站 id
-		if station_name and not station_id:
-			station_id = self.get_station_id(station_name)
-
-		# 若輸入 觀測站 id，但未輸入 觀測站名稱，會用 觀測站 id 找 觀測站名稱
-		if station_id and not station_name:
-			station_name = self.get_station_name(station_id)
-
-		stname = self.get_stname(station_name)
-		return station_id, station_name, stname
 
 	# 用 觀測站 id 找到觀測站的所在縣市
 	def get_station_location(self, station_id):
@@ -67,24 +52,14 @@ class Climate_Station:
 		station_area = '{}-{}'.format(station_location, station_name)
 		return station_area
 
-	# e.g. get_month_periods(start_period='2017-11', end_period='2018-2')
-	# output: ['2017-11', '2017-12', '2018-01']
-	def get_month_periods(self, start_period, end_period):
-		return pd.date_range(start=start_period, end=end_period, freq='MS').strftime('%Y-%m').tolist()
-
-	# e.g. get_day_periods(start_period='2017-12-30', end_period='2018-01-02')
-	# output: ['2017-12-30', '2017-12-31', '2018-01-01', '2018-01-02']
-	def get_day_periods(self, start_period, end_period):
-		return pd.date_range(start=start_period, end=end_period, freq='D').strftime('%Y-%m-%d').tolist()
+	def get_full_url(self, sub_url, station_id, period):
+		stname = self.dataFrame.loc[station_id]['stname']
+		return '{}/{}&station={}&stname={}&datepicker={}'.format(self.base_url, sub_url, station_id, stname, period)
 
 	# e.g. get_daily_full_url(period='2017-12', station_id='466910')
-	def get_daily_full_url(self, period, station_id=None, station_name=None):
-		station_id, station_name, stname = self.get_station(station_id, station_name)
-		daily_url = 'MonthDataController.do?command=viewMain'
-		return '{}/{}&station={}&stname={}&datepicker={}'.format(self.base_url, daily_url, station_id, stname, period)
+	def get_daily_full_url(self, period, station_id):
+		return self.get_full_url(self.daily_url, station_id, period)
 
 	# e.g. get_hourly_full_url(period='2017-12-30', station_id='466910')
-	def get_hourly_full_url(self, period, station_id=None, station_name=None):
-		station_id, station_name, stname = self.get_station(station_id, station_name)
-		month_url = 'DayDataController.do?command=viewMain'
-		return '{}/{}&station={}&stname={}&datepicker={}'.format(self.base_url, month_url, station_id, stname, period)
+	def get_hourly_full_url(self, period, station_id):
+		return self.get_full_url(self.hourly_url, station_id, period)
