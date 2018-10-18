a1fc9b93ddd4b3bea5f37785085d63fbb32ca9fb
titangene <titangene.tw@gmail.com>
2018-10-18 16:14:05 +0800

    refactor: main

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from lib.Climate_Crawler import Climate_Crawler
 def start():
 	# 更新目前可用的氣候觀測站
 	Station_Crawler().start()
-
-	climate_crawler = Climate_Crawler()
 	# 抓氣候資料，包括 daily 和 hourly 的氣候資料
-	climate_crawler.start()
+	Climate_Crawler().start()
 
 def main():
 	start()
